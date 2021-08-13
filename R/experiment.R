ae09a09635bd4eda8378dd36be24737026ac3e37
Tiffany Tang <tmtang15@gmail.com>
2021-08-13 16:34:57 -0700

    fix absolute paths

--- R/experiment.R
+++ R/experiment.R
@@ -142,7 +142,7 @@ Experiment <- R6::R6Class(
           save_dir <- file.path(parent$save_dir, name)
         }
       }
-      self$save_dir <- save_dir
+      self$save_dir <- R.utils::getAbsolutePath(save_dir)
     },
     run = function(parallel_strategy = c("reps", "dgps", "methods", "dgps+methods"),
                    trial_run = FALSE, save = FALSE, ...) {
@@ -268,7 +268,7 @@ Experiment <- R6::R6Class(
         write.csv(NULL, file = fname, quote = F)
       }
       
-      descendants <- map(list.dirs(self$save_dir),
+      descendants <- map(list.dirs(save_dir),
                          function(d) {
                            if (file.exists(file.path(d, "experiment.rds"))) {
                              return(readRDS(file.path(d, "experiment.rds")))
@@ -293,10 +293,11 @@ Experiment <- R6::R6Class(
       }
       return(invisible(self))
     },
-    create_rmd = function(save_dir = file.path(getwd(), self$save_dir)) {
+    create_rmd = function(...) {
+      self$create_doc_template()
       input_fname <- system.file("rmd", "results.Rmd", package = packageName())
-      output_fname <- file.path(save_dir, paste0(self$name, ".html"))
-      params_list <- list(sim_name = self$name, sim_path = save_dir)
+      output_fname <- file.path(self$save_dir, paste0(self$name, ".html"))
+      params_list <- list(sim_name = self$name, sim_path = self$save_dir)
       rmarkdown::render(input = input_fname, 
                         params = params_list,
                         output_file = output_fname)
@@ -513,18 +514,13 @@ vary_across <- function(experiment, dgp = NULL, method = NULL,
 }
 
 #' @export
-create_rmd <- function(experiment_name, experiment_dirname) {
-  if (!file.exists(file.path(experiment_dirname, "experiment.rds"))) {
-    stop(sprintf("No experiment found in %s", experiment_dirname))
+create_rmd <- function(experiment, experiment_dirname) {
+  if (missing(experiment) & missing(experiment_dirname)) {
+    stop("Must provide argument for one of experiment or experiment_dirname")
   }
-  experiment_dirname <- file.path(getwd(), experiment_dirname)
-  input_fname <- system.file("rmd", "results.Rmd", package = packageName())
-  output_fname <- file.path(experiment_dirname, 
-                            paste0(experiment_name, ".html"))
-  params_list <- list(sim_name = experiment_name, sim_path = experiment_dirname)
-  rmarkdown::render(input = input_fname, 
-                    params = params_list,
-                    output_file = output_fname)
-  output_fname <- str_replace_all(output_fname, " ", "\\\\ ")
-  system(paste("open", output_fname))
+  if (missing(experiment)) {
+    experiment <- readRDS(file.path(experiment_dirname, "experiment.rds"))
+  }
+  experiment$create_doc_template()
+  experiment$create_rmd()
 }
