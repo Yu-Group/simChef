79b42078e6fb1d0c4bed39afdeb453feb06174c3
Tiffany Tang <tiffany.tang@berkeley.edu>
2022-12-01 17:02:31 -0800

    minor bug fix

--- tests/testthat/test-utils-rmd.R
+++ tests/testthat/test-utils-rmd.R
@@ -1,7 +1,7 @@
 test_that("pasteMd works properly", {
 
   expect_error(pasteMd())
-  expect_error(pasteMd("path/to/file"))
+  expect_warning(expect_error(pasteMd("path/to/file")))
 
   filename <- file.path("md", "test_doc.md")
   expect_snapshot_output(pasteMd(filename))
