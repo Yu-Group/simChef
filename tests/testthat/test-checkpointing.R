c078cd2c1406d7ab74935e282c7bc9b3246b1975
Tiffany Tang <tiffany.tang@berkeley.edu>
2022-12-23 13:18:08 -0600

    fix cross warning in tests

--- tests/testthat/test-checkpointing.R
+++ tests/testthat/test-checkpointing.R
@@ -58,7 +58,7 @@ withr::with_tempdir(pattern = "simChef-test-checkpointing-temp", code = {
 
     vary_results <- mapply(list, distinct_results$param1, distinct_results$param2,
                            distinct_results$vec, SIMPLIFY = FALSE)
-    expected_vary_results <- purrr::cross(
+    expected_vary_results <- cross(
       list(c(1,2), c(3,4), list(c(1,3,4), 2:7))
     )
 
