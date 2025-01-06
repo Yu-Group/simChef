5f7844b8f96a4d0b289cd9fe2ea538823d526dd9
Tiffany Tang <tiffany.tang@berkeley.edu>
2025-01-06 12:05:26 -0600

    Fix render_docs when save_in_bulk = FALSE (#200)
    * add `get_save_in_bulk()` method to `Experiment` class
    * update render_docs to work when `save_in_bulk = FALSE`

--- R/experiment.R
+++ R/experiment.R
@@ -1121,8 +1121,10 @@ Experiment <- R6::R6Class(
       } else {
         save_dir <- private$.get_vary_across_dir()
       }
-      if (!dir.exists(file.path(save_dir, "fit_results"))) {
-        dir.create(file.path(save_dir, "fit_results"), recursive = TRUE)
+      if (!save_in_bulk) {
+        if (!dir.exists(file.path(save_dir, "fit_results"))) {
+          dir.create(file.path(save_dir, "fit_results"), recursive = TRUE)
+        }
       }
 
       dgp_list <- private$.get_obj_list("dgp")
@@ -2269,6 +2271,18 @@ Experiment <- R6::R6Class(
       }
       saveRDS(self, file.path(save_dir, "experiment.rds"))
       invisible(self)
+    },
+
+    #' @description Get the `save_in_bulk` parameter for the `Experiment`.
+    #'
+    #' @return Logical, indicating whether the results are saved in bulk or not.
+    get_save_in_bulk = function() {
+      save_in_bulk <- private$.save_in_bulk
+      if (is.null(save_in_bulk)) {
+        # for experiments created before save_in_bulk was introduced
+        save_in_bulk <- c(fit = TRUE, eval = TRUE, viz = TRUE)
+      }
+      return(save_in_bulk)
     },
 
     #' @description Export all cached `Visualizer` results from an
